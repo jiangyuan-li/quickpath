ff29db18591276f915660cef076cbc1187a72456
Jiangyuan Li <jiangyuanli@tamu.edu>
2019-11-28 01:11:32 -0600

    implement preprocossing function

--- R/extract_genes_by_pathway.R
+++ R/extract_genes_by_pathway.R
@@ -1,44 +1,80 @@
-#' title
+#' Extract DEGs from pathways
 #'
-#'@param
-#'@param
-#'@return
+#'@param pathway A vector contains interested pathwat names
+#'@param deg A dataframe contains DEGS information
+#'@param class Specify which type of gene
+#'@param out.name A file name ends with ".xlsx", if specified, the output will be written into this file
+#'@returnn A list of DEGS for each pathway
 #'@examples
-#'eg
+#'pathway <- c("Purine metabolism", "PI3K-Akt signaling pathway", "AMPK signaling pathway", "Choline metabolism in cancer")
+#'deg = grab_deg_from_cuffdiff(gene_exp.diff)
+#'deg.list = extract_degs_by_pathway(pathway, deg, class = "mmu")
 #'@export
 
-extract_genes_by_pathway <- function(pathway, deg.file, out.name, class=c("mmu","hsa","gga")){
+extract_degs_by_pathway <- function(pathway, deg, class = c("mmu","hsa","gga"), out.name = NULL){
 
-  ref.paths = eval(parse(text = paste0(class,"_pathway")))
-  row.names(ref.paths) <- NULL
-  path.ids <- c()
-  for(i in 1:length(pathway)){
-    print(ref.paths[grep(pathway[i],ref.paths[,2],ignore.case = TRUE),])
-    print(i)
-    path.ids[i] <- ref.paths[grep(pathway[i],ref.paths[,2],ignore.case = TRUE),1]
-  }
-
-  #all <- eval(parse(text = paste0("genes_pathways_",class)))
+  n = length(pathway)
+  path.ids <- check_pathway_name(pathway, class = class)
 
   path_list <- eval(parse(text = paste0(class,"_path_gene_list")))
-  wb = xlsx::createWorkbook()
 
-  for (i in 1:length(pathway)){
-    sheet = createSheet(wb, pathway[i])
-
+  out.list <- list()
+  for(i in 1:n){
     symbol = as.character(path_list[[path.ids[i]]][,'gene.symbol'])
+    symbol = symbol[!is.na(symbol)]
 
-    out.deg <- deg[deg$gene%in%symbol,]
-    print(nrow(out.deg))
-    print(length(symbol))
+    out.deg <- deg[deg$external_gene_name%in%symbol,]
+    out.list[[i]] <- out.deg
 
-
-    print(path.ids[i])
-    addDataFrame(out.deg, sheet=sheet, startRow =1, row.names=FALSE)
-
-
+    print(paste0(pathway[i]," has DEGs ",nrow(out.deg),"/",length(symbol)))
   }
+  if(!is.null(out.name)){
+    message("Writing the output to a xlsx file...")
+    wb = xlsx::createWorkbook()
 
-  xslx::saveWorkbook(wb, out.name)
+    for (i in 1:length(pathway)){
+      sheet = createSheet(wb, pathway[i])
+      print(path.ids[i])
+      addDataFrame(out.deg, sheet=sheet, startRow =1, row.names=FALSE)
+    }
+    xslx::saveWorkbook(wb, out.name)
+    message("End writing...")
+  }
+  return(out.list)
+}
 
+
+####################check pathway name and return path ids ##############
+#' Extract DEGs from pathways
+#'
+#'@param pathway A vector contains interested pathwat names
+#'@param class Specify which type of gene
+#'@return
+#'@examples Path IDs for each pathway
+#'pathway <- c("Purine metabolism", "PI3K-Akt signaling pathway", "AMPK signaling pathway", "Choline metabolism in cancer")
+#'path.ids = check_pathway_name(pathway, class = "mmu")
+#'@export
+check_pathway_name <- function(pathway, class = c("mmu","hsa","gga")){
+
+  all.paths = eval(parse(text = paste0(class,"_pathway")))
+  ref.paths = all.paths[,2]
+  names(ref.paths) = all.paths[,1]
+
+  path.ids <- c()
+  n = length(pathway)
+
+  message("Start checking pathway names...")
+  for(i in 1:n){
+    print(paste0(i,"/",n))
+    ind = grep(pathway[i],ref.paths,ignore.case = TRUE)
+    tmp.info = ref.paths[ind]
+    print(tmp.info)
+    if(length(ind) != 1){
+      stop("Input contains ambiguous or invalid pathway name!")
+    }
+    path.ids[i] <- names(tmp.info)
+  }
+  message("All names are valid!")
+
+  return(path.ids)
 }
--- R/grab_deg_from_cuffdiff.R
+++ R/grab_deg_from_cuffdiff.R
@@ -1,11 +1,31 @@
-#' title
+#' Grab differential expressed genes (DEGs) from cuffdiff result
 #'
-#'@param
-#'@param
-#'@return
+#'@param gene_exp.diff a dataframe from cuffdiff result
+#'@param out.name If specified, the output will be written to this file.
+#'@param criterion p_value or q_value
+#'@param cutoff significance level
+#'@return A dataframe contains all DEGs with gene symvbols.
 #'@examples
-#'eg
+#'head(gene_exp.diff)
+#'deg = grab_deg_from_cuffdiff(gene_exp.diff)
+#'head(deg)
 #'@export
-grab_deg_from_cuffdiff <- function(gene_exp.diff, criterion = c("pval","qval"), cutoff = 0.5){
+grab_deg_from_cuffdiff <- function(gene_exp.diff, out.name = NULL, class = c("mmu","gga","hsa"), criterion = c("p_value","q_value"), cutoff = 0.05){
 
+  ############## get DEGs ##################
+  deg <- gene_exp.diff[gene_exp.diff[,criterion] <= cutoff, ]
+  ############## get gene symbol ##############
+  mart <- eval(parse(text = paste0("mart_",class)))
+  symbol = biomaRt::getBM(attributes=c("ensembl_gene_id", "external_gene_name","description"),
+                 filters="ensembl_gene_id", values=deg$gene_id, mart=mart)
+  names(symbol)[1] <- "gene_id"
+
+  ############## combine symbol info and expression info #######
+  out <- merge(deg,symbol,by="gene_id",all.x = T)
+
+  if(!is.null(out.name)){
+    write.csv(out, out.name, row.names = FALSE)
+  }
+
+  return(out)
 }
