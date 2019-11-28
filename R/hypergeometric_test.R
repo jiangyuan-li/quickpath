c885e90851d165e42a1bf8857d2e6e14ad888b83
Jiangyuan Li <jiangyuanli@tamu.edu>
2019-11-28 00:58:38 -0600

    implement parallel inference function

--- R/hypergeometric_test.R
+++ R/hypergeometric_test.R
@@ -2,73 +2,47 @@
 #'
 #'@param sig.genes A vector of significantly changed genes
 #'@param class Specify which type of gene
+#'@param mc.cores Specify how many cores to use
 #'@return A list of all significantly changed pathways, and all pathways with p value
 #'@examples
-#'pathway_analysis(c("Osr1","Tbx5"),"mmu")
+#'deg = grab_deg_from_cuffdiff(gene_exp.diff)
+#'sig.genes = deg$external_gene_name
+#'path_res = pathway_analysis(sig.genes, class = "mmu")
 #'@export
-pathway_analysis <- function(sig.genes,class=c("mmu","hsa","gga")){
-  library(readr)
-  pvalhyper <- function(all.genes, sig.genes, gs){
-    N = length(all.genes)
-    n = length(gs)
-    R = length(sig.genes)
-    r = length(intersect(gs, sig.genes))
-    pval <- sum(dhyper(0:r,R,N-R,n))
+pathway_analysis <- function(sig.genes, class=c("mmu","hsa","gga"), mccores = 8){
 
-    #if(r==0){
-    #  return(c(pval,r/n))
-    #}
-    #else{
-    #  return(c(min(c(pval,1-pval)),r/n))
-    #}
+  ############## prepare needed data #################
+  all = eval(parse(text = paste0("genes_pathways_",class)))
+  paths = eval(parse(text = paste0(class,"_pathway")))
+  path.gene = eval(parse(text = paste0(class,"_path_gene_list")))
 
-    if(r==0){
-      return(c(NA,r/n))
-    }
-    else{
-      return(c(1-pval,r/n))
-    }
-  }
-
-  if(class == "mmu"){
-    all <- read.csv("~/Biodatabase/Pathway/genes_pathways_mmu.csv")
-    paths <- read.csv("~/Biodatabase/Pathway/mmu_pathway.csv")
-    path.gene <- readRDS("~/Biodatabase/Pathway/mmu_path_gene_list.rds")
-    print(class)
-
-  }
-  if(class == "hsa"){
-    all <- read.csv("~/Biodatabase/Pathway/genes_pathways_hsa.csv")
-    paths <- read.csv("~/Biodatabase/Pathway/hsa_pathway.csv")
-    path.gene <- readRDS("~/Biodatabase/Pathway/hsa_path_gene_list.rds")
-    print(class)
-
-  }
-  if(class == "gga"){
-    all <- read.csv("~/Biodatabase/Pathway/genes_pathways_gga.csv")
-    paths <- read.csv("~/Biodatabase/Pathway/gga_pathway.csv")
-    path.gene <- readRDS("~/Biodatabase/Pathway/gga_path_gene_list.rds")
-    print(class)
-
-  }
-  all.genes <- unique(all$gene.symbol)
+  ############# clean background gene list ############
+  all.genes <- all$gene.symbol
   all.genes <- all.genes[!is.na(all.genes)]
-  all.genes <- all.genes[!all.genes==""]
+  all.genes <- unique(all$gene.symbol)
 
+  ############ clean DEGs list #################
+  sig.genes <- sig.genes[!is.na(sig.genes)]
+  sig.genes <- unique(sig.genes)
   sig.genes <- unique(intersect(sig.genes,all.genes))
-  print(paste("sig genes in pathway:",length(sig.genes)))
+  ###########################################
+
+  print(paste0("sig genes in pathway: ",length(sig.genes),"/",length(all.genes)))
+
+  ################### do hypergeometric test ###########
   out <- parallel::mclapply(path.gene,function(x){
     if(!is.na(x)){
       symbol <- na.omit(x[,1])
-      symbol = symbol[!symbol==""]
+      symbol = symbol[!is.na(symbol)]
       pval.percent = pvalhyper(all.genes,sig.genes,symbol)
       return(pval.percent)
     }
     else{
       return(c(NA,NA))
     }
-  },mc.cores = 8)
-
+  },mc.cores = mccores)
+  print("Inference part is Done.")
+  ############## format results ##################
   tmpname <- names(out)
   out <- t(as.data.frame(out))
   colnames(out) <- c("pval","percentage")
@@ -82,4 +56,21 @@ pathway_analysis <- function(sig.genes,class=c("mmu","hsa","gga")){
   return(list(out[out$pval<=0.05,],
               out))
 
+}
+
+############## get pvalue from hypergeometric distribution ############
+pvalhyper <- function(all.genes, sig.genes, gs){
+  N = length(all.genes)
+  R = length(sig.genes)
+
+  n = length(gs)
+  r = length(intersect(gs, sig.genes))
+
+  if(r==0){
+    return(c(1,r/n))
+  }
+  else{
+    pval <- 1 - sum(dhyper(0:r-1,R,N-R,n))
+    return(c(pval,r/n))
+  }
 }
