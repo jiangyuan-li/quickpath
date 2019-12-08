29c984978d4eb61ecb41f71a0ee1ded7418280ef
Jiangyuan Li <jiangyuanli@tamu.edu>
2019-12-07 21:55:51 -0600

    inlcude changes in vignettes

--- doc/tutorial.R
+++ doc/tutorial.R
@@ -11,7 +11,7 @@ library(quickpath)
 data(gene_exp.diff)
 print(dim(gene_exp.diff))
 head(gene_exp.diff, n = 5)
-deg = grab_deg_from_cuffdiff(gene_exp.diff, class = "mmu", criterion = "p_value", cut.off = 0.05)
+deg = grab_degs_from_cuffdiff(gene_exp.diff, class = "mmu", criterion = "p_value", cut.off = 0.05)
 print(dim(deg))
 head(deg[,c(1,4,8,9,10,11,12,13,15,16)],n = 5)
 
@@ -21,7 +21,8 @@ path_res = pathway_analysis(sig.genes, class = "mmu")
 head(path_res[[1]])
 
 ## ---- echo = T----------------------------------------------------------------
-deg_egg = grab_deg_from_cuffdiff(gene_exp.diff_egg, class = "gga")
+data(gene_exp.diff_egg)
+deg_egg = grab_degs_from_cuffdiff(gene_exp.diff_egg, class = "gga")
 sig.genes = deg_egg$external_gene_name
 path_res_egg = pathway_analysis(sig.genes, class = "gga")
 
@@ -30,16 +31,21 @@ pathway <- c("Purine metabolism", "PI3K-Akt signaling pathway",
              "AMPK signaling pathway", "Choline metabolism in cancer")
 deg.list = extract_genes_by_pathway(pathway, deg, class = "mmu")
 
-## ---- echo = T----------------------------------------------------------------
-deg_chow = grab_deg_from_cuffdiff(gene_exp.diff_chow, class = "mmu")
+## ---- message = F, echo = T---------------------------------------------------
+data(gene_exp.diff_chow)
+deg_chow = grab_degs_from_cuffdiff(gene_exp.diff_chow, class = "mmu")
 sig.genes = deg_chow$external_gene_name
 path_res_chow = pathway_analysis(sig.genes, class = "mmu")
 
-deg_e105 = grab_deg_from_cuffdiff(gene_exp.diff_e105, class = "mmu")
+## ---- message = F, echo = T---------------------------------------------------
+data(gene_exp.diff_e105)
+deg_e105 = grab_degs_from_cuffdiff(gene_exp.diff_e105, class = "mmu")
 sig.genes = deg_e105$external_gene_name
 path_res_e105 = pathway_analysis(sig.genes, class = "mmu")
 
-deg_e95 = grab_deg_from_cuffdiff(gene_exp.diff_e95, class = "mmu")
+## ---- message = F, echo = T---------------------------------------------------
+data(gene_exp.diff_e95)
+deg_e95 = grab_degs_from_cuffdiff(gene_exp.diff_e95, class = "mmu")
 sig.genes = deg_e95$external_gene_name
 path_res_e95 = pathway_analysis(sig.genes, class = "mmu")
 
@@ -53,7 +59,7 @@ fig_path(path.ids, list.info, group.info, criterion = "pval", path.names = path.
 
 ## ---- message = F, echo = T---------------------------------------------------
 total.genes = mmu_genes_pathways$gene.symbol
-deg = grab_deg_from_cuffdiff(gene_exp.diff, class = "mmu", criterion = "p_value", cut.off = 0.05)
+deg = grab_degs_from_cuffdiff(gene_exp.diff, class = "mmu", criterion = "p_value", cut.off = 0.05)
 sig.genes = deg$external_gene_name
 meth_path_res = pathway_analysis_meth(sig.genes,total.genes, class = "mmu")
 
